b79b40527b25182c9d56da68e7c19de79f5a3975
Divindjorly <divindjorly.poadzola@esprit.tn>
2025-07-22 14:53:00 +0100

    Mon premier commit

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -1,37 +1,56 @@
+//src/constants/items.js
 import React from 'react';
-import {
-  Sofa,
-  Bed,
-  Tv,
-  Fridge,
-  WashingMachine,
-  Book,
-  Desk,
-  Drawer,
-  Shelf,
-  Lamp,
-  AirConditioner,
-  ForkKnife,
-  Bath,
-  Chair,
-  Table,
-} from 'iconoir-react';
+/*import {
+  Weekend,              // canapé
+  Bed,                  // lit
+  ChairAlt,             // chaise/fauteuil
+  TableBar,             // table
+  Tv,                   // tv
+  Kitchen,              // frigo
+  LocalLaundryService,  // lave-linge
+  Book,                 // bibliothèque/armoire
+  Desk,                 // bureau/console
+  Inventory2,           // commode
+  StoreMallDirectory,   // étagère
+  Light,                // lampe
+  AcUnit,               // clim / ventilateur
+  Restaurant,           // vaisselle
+  Bathtub,              // baignoire
+} from '@mui/icons-material';*/
+
+// ✅ Import du fichier SVG comme composant React
+import { ReactComponent as CanapeSvg } from './items/canapé.svg';
+import { ReactComponent as FauteuilSvg } from './items/fauteuil.svg';
+import canapeImg from './items/bed (1).png';
+import tableImg from './items/dining-table.png';
+import tvImg from './items/tv.svg';
+import biblioImg from './items/biblio.svg';
+import commodeImg from './items/tablelit.svg';
+import chairImg from './items/chaise.svg';
+import fridgeImg from './items/fridge.png';
+import etagereImg from './items/6038033_bedside table_book_bookshelf_furnitures_house_icon.svg';
+import washingMImg from './items/washing-machine.png';
+import climImg from './items/clim.png';
+import baignoireImg from './items/Baignoire.svg';
+import vaisselleImg from './items/vaiselle.jpg';
+import bureauImg from './items/bureau.png';
+import lampeImg from './items/lampe.png';
 
 export const ITEMS = [
-  { name: 'canape',    label: 'Canapé',            category: 'Salon',        icon: <Sofa width={36} height={36} /> },
-  { name: 'lit',       label: 'Lit double',        category: 'Chambre',      icon: <Bed width={36} height={36} /> },
-  { name: 'fauteuil',  label: 'Fauteuil',          category: 'Salon',        icon: <Chair width={36} height={36} /> },
-  { name: 'table',     label: 'Table à manger',    category: 'Cuisine',      icon: <Table width={36} height={36} /> },
-  { name: 'chaise',    label: 'Chaise',            category: 'Cuisine',      icon: <Chair width={36} height={36} /> },
-  { name: 'tv',        label: 'Télévision',        category: 'Salon',        icon: <Tv width={36} height={36} /> },
-  { name: 'frigo',     label: 'Réfrigérateur',     category: 'Cuisine',      icon: <Fridge width={36} height={36} /> },
-  { name: 'laveLinge', label: 'Lave-linge',        category: 'Buanderie',    icon: <WashingMachine width={36} height={36} /> },
-  { name: 'armoire',   label: 'Armoire / bibliothèque', category: 'Chambre', icon: <Book width={36} height={36} /> },
-  { name: 'bureau',    label: 'Bureau',            category: 'Bureau',       icon: <Desk width={36} height={36} /> },
-  { name: 'commode',   label: 'Commode',           category: 'Chambre',      icon: <Drawer width={36} height={36} /> },
-  { name: 'etagere',   label: 'Étagère',           category: 'Salon',        icon: <Shelf width={36} height={36} /> },
-  { name: 'lampe',     label: 'Lampe',             category: 'Tous',         icon: <Lamp width={36} height={36} /> },
-  { name: 'ventilo',   label: 'Climatiseur / Ventilateur', category: 'Tous', icon: <AirConditioner width={36} height={36} /> },
-  { name: 'vaisselle', label: 'Service vaisselle', category: 'Cuisine',      icon: <ForkKnife width={36} height={36} /> },
-  { name: 'baignoire', label: 'Baignoire',         category: 'Salle de bain', icon: <Bath width={36} height={36} /> },
+  { name: 'canape',    label: 'Canapé',            category: 'Salon',     icon: <CanapeSvg style={{ width: 50, height: 50 }} /> },
+  { name: 'lit',       label: 'Lit double',        category: 'Chambre',   icon: <img src={canapeImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'fauteuil',  label: 'Fauteuil',          category: 'Salon',     icon: <FauteuilSvg style={{ width: 50, height: 50 }} /> },
+  { name: 'table',     label: 'Table à manger',    category: 'Cuisine',   icon: <img src={tableImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'chaise',    label: 'Chaise',            category: 'Cuisine',   icon: <img src={chairImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'tv',        label: 'Télévision',        category: 'Salon',     icon: <img src={tvImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'frigo',     label: 'Réfrigérateur',     category: 'Cuisine',   icon: <img src={fridgeImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'laveLinge', label: 'Lave-linge',        category: 'Buanderie', icon: <img src={washingMImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'armoire',   label: 'Armoire / bibliothèque', category: 'Chambre', icon: <img src={biblioImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'bureau',    label: 'Bureau',            category: 'Bureau',    icon: <img src={bureauImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'commode',   label: 'Commode',           category: 'Chambre',   icon: <img src={commodeImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'etagere',   label: 'Étagère',           category: 'Salon',     icon: <img src={etagereImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'lampe',     label: 'Lampe',             category: 'Tous',      icon: <img src={lampeImg} style={{ width: 90, height: 50 }} /> },
+  { name: 'ventilo',   label: 'Climatiseur / Ventilateur', category: 'Tous', icon: <img src={climImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'vaisselle', label: 'Service vaisselle', category: 'Cuisine',   icon: <img src={vaisselleImg} style={{ width: 50, height: 50 }} /> },
+  { name: 'baignoire', label: 'Baignoire',         category: 'Salle de bain', icon: <img src={baignoireImg} style={{ width: 50, height: 50 }} />},
 ];
